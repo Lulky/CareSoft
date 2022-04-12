4a6677dd64daf8d0103f6c77f85eac85da6c2f73
unknown <lukyluxiano.12@gmail.com>
2022-04-11 22:16:32 -0500

    CareSoft

---
+++ src/com/caresoft/clinicapp/UsuarioAdmin.java
@@ -0,0 +1,90 @@
+package com.caresoft.clinicapp;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class UsuarioAdmin extends Usuario implements UsuarioCompatibleConHIPAA, AdminCompatibleConHIPAA {
+
+
+    // Dentro de la clase:
+    private Integer employeeID;
+    private String role;
+    private ArrayList<String> securityIncidents = new ArrayList<String>();
+    
+    // PARA HACER: Implementar un constructor que tome un ID y un rol
+    
+    public UsuarioAdmin(Integer id, String role) {
+		super(id);
+		
+	}
+    @Override
+	public boolean assignPin(int pin) {
+		if(pin > 99999 ) {
+			return true;
+			} else {
+			return false;
+			}	
+	}
+    
+	@Override
+	public boolean accessAuthorized(Integer confirmedAuthID) {
+		if(this.id == confirmedAuthID) {
+		return true;
+		}else {
+		
+		this.authIncident();
+		return false;
+		}
+	}		
+	
+	@Override
+	public ArrayList<String> reportSecurityIncidents() {
+		return securityIncidents;
+	}
+    
+    public void newIncident(String notes) {
+        String report = String.format(
+            "Fecha y hora de envío: %s \n, Reportado por ID: %s\n Notas: %s \n", 
+            new Date(), this.id, notes
+        );
+        securityIncidents.add(report);
+    }
+    public void authIncident() {
+        String report = String.format(
+            "Fecha y hora de envío: %s \n, ID: %s\n Notas: %s \n", 
+            new Date(), this.id, "FALLÓ EL INTENTO DE AUTORIZACIÓN PARA ESTE USUARIO"
+        );
+        securityIncidents.add(report);
+    }
+
+    
+    // PARA HACER: Setters y Getters
+	public Integer getEmployeeID() {
+		return employeeID;
+	}
+
+	public void setEmployeeID(Integer employeeID) {
+		this.employeeID = employeeID;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public ArrayList<String> getSecurityIncidents() {
+		
+		return securityIncidents;
+	}
+
+	public void setSecurityIncidents(ArrayList<String> securityIncidents) {
+		this.securityIncidents = securityIncidents;
+	}
+    
+    
+    
+    
+}
